a322d34e20fd722253f308995ac4158f01650163
Ladislav Prskavec <ladislav@prskavec.net>
2015-08-24 08:47:07 +0200

    Example gofmt parser

--- parser.go
+++ parser.go
@@ -1,5 +1,56 @@
 package main
 
+// http://godoc.org/github.com/kr/logfmt#example-package--CustomHandler
+import (
+	"bytes"
+	"fmt"
+	"github.com/kr/logfmt"
+	"log"
+	"strconv"
+)
+
+type Measurement struct {
+	Key  string
+	Val  float64
+	Unit string // (e.g. ms, MB, etc)
+}
+
+type Measurements []*Measurement
+
+var measurePrefix = []byte("measure.")
+
+func (mm *Measurements) HandleLogfmt(key, val []byte) error {
+	if !bytes.HasPrefix(key, measurePrefix) {
+		return nil
+	}
+	i := bytes.LastIndexFunc(val, isDigit)
+	v, err := strconv.ParseFloat(string(val[:i+1]), 10)
+	if err != nil {
+		return err
+	}
+	m := &Measurement{
+		Key:  string(key[len(measurePrefix):]),
+		Val:  v,
+		Unit: string(val[i+1:]),
+	}
+	*mm = append(*mm, m)
+	return nil
+}
+
+// return true if r is an ASCII digit only, as opposed to unicode.IsDigit.
+func isDigit(r rune) bool {
+	return '0' <= r && r <= '9'
+}
+
 func ProcessLine(line string, prefix string) string {
-	return string(prefix + "." + line)
+	var data = []byte("measure.a=1ms measure.b=10 measure.c=100MB measure.d=1s garbage")
+
+	mm := make(Measurements, 0)
+	if err := logfmt.Unmarshal(data, &mm); err != nil {
+		log.Fatalf("err=%q", err)
+	}
+	for _, m := range mm {
+		fmt.Printf("%v\n", *m)
+	}
+	return "test"
 }
